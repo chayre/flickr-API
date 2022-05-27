0db24b41478d0657829e77d0d13d6c3204b2ea7b
seairs <sea.airs77@gmail.com>
2022-05-26 22:59:05 -0400

    configuring API calls

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,2 +1,18 @@
 class StaticPagesController < ApplicationController
+  require 'flickr'
+
+  def home
+    # Wait for user to submit user ID in params
+    unless params[:id].nil?
+      #  Format, flickr gem: flickr = Flickr.new "YOUR API KEY", "YOUR SHARED SECRET"
+      flickr = Flickr.new ENV['flickr_key'], ENV['flickr_secret']
+      begin
+        # Get photos and store in @photos
+        @photos = flickr.photos.search(user_id: params[:id], api_key: ENV['flickr_key'], per_page: 20)
+      rescue Flickr::FailedResponse
+        # Flash alert for FailedResponse
+        flash[:alert] = 'User not found.'
+      end
+    end
+  end
 end
